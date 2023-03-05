b7eb4e1113ab994e1b35be9724152fb9e4591d2e
itorr <itorrrrrr@me.com>
2023-03-05 21:33:59 +0800

    config edit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,22 @@
 module.exports = {
     title: 'Hi, WebStation',
-    description: 'WebStation Help Center'
+    description: 'WebStation Help Center',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            {
+                text: '知识库',
+                items: [
+                    { text: 'FAQ', link: '/a' },
+                    { text: 'HowTo', link: '/b' }
+                ]
+            },
+            {
+                text: '了解更多',
+                items: [
+                    { text: '官网', link: 'https://enazo.cn' }
+                ]
+            }
+        ]
+    }
 }
